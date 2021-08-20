f97b32dc2db836a0cb4db0f20b0d41a2fdd640c9
xilongwang <1595309412@qq.com>
2021-08-20 16:16:40 +0800

    添加x-layout组件

---
+++ src/components/component/XLayout.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  props: {},
+  // 免去的转译的过程
+  render(h, params) {
+    return h(
+      "section",
+      {
+        class: {
+          XLayout: true,
+        },
+      },
+      this.handleSlot(this.$slots.default)
+    );
+  },
+  methods: {
+    handleSlot(slot) {
+      if (slot && slot.length > 0) {
+        return slot[0].text;
+      }
+      // return this.text || '默认按钮';
+      return ''
+    },
+  },
+};
+</script>
+<style scoped>
+.x-layout {
+  display: flex;
+  flex: auto;
+  min-height: 0;
+  flex-direction: column;
+}
+</style>
+  
